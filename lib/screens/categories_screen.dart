7d75f120db5bf12b28961fe2ceb6586eec8d798a
dainavi.purav <dainavi.purav@cmss.in>
2024-04-17 22:04:17 +0530

    added category item ui

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:udemy_005_meals_app/dummy_data.dart';
+import 'package:udemy_005_meals_app/widgets/category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryItem(
+              category: category,
+            ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:udemy_005_meals_app/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({super.key, required this.category});
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.55),
+            category.color.withOpacity(0.9)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(16),
+        ),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+      ),
+    );
+  }
+}
